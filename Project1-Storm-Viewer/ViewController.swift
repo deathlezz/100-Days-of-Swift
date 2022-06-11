141953c8ff8ce7ee26e1669ee6a2a4be40b3921a
deathlezz <deathlezz@wp.pl>
2022-06-11 19:07:15 +0100

    Designing our interface.

--- Project1-Storm-Viewer/ViewController.swift
+++ Project1-Storm-Viewer/ViewController.swift
@@ -7,7 +7,7 @@
 
 import UIKit
 
-class ViewController: UIViewController {
+class ViewController: UITableViewController {
     
     var pictures = [String]()
 
@@ -26,6 +26,16 @@ class ViewController: UIViewController {
             }
         }
         print(pictures)
+    }
+    
+    override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return pictures.count
+    }
+    
+    override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = tableView.dequeueReusableCell(withIdentifier: "Picture", for: indexPath)
+        cell.textLabel?.text = pictures[indexPath.row]
+        return cell
     }
 }
 
