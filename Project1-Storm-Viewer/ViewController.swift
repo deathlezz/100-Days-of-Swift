d398343226743e510d97c0d510583c46244bb957
deathlezz <deathlezz@wp.pl>
2022-06-11 18:24:58 +0100

    Listing images with FileManager.

--- Project1-Storm-Viewer/ViewController.swift
+++ Project1-Storm-Viewer/ViewController.swift
@@ -8,12 +8,24 @@
 import UIKit
 
 class ViewController: UIViewController {
+    
+    var pictures = [String]()
 
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
+        
+        let fm = FileManager.default
+        let path = Bundle.main.resourcePath!
+        let items = try! fm.contentsOfDirectory(atPath: path)
+        
+        for item in items {
+            if item.hasPrefix("nssl") {
+                // this is a picture to load!
+                pictures.append(item)
+            }
+        }
+        print(pictures)
     }
-
-
 }
 
