07eee1e3b96bb0b935c591bfabfb7810b79aa851
deathlezz <deathlezz@wp.pl>
2022-06-12 20:11:48 +0100

    Loading images with UIImage

--- Project1-Storm-Viewer/DetailViewController.swift
+++ Project1-Storm-Viewer/DetailViewController.swift
@@ -10,11 +10,16 @@ import UIKit
 class DetailViewController: UIViewController {
     
     @IBOutlet var imageView: UIImageView!
+    var selectedImage: String?
     
     override func viewDidLoad() {
         super.viewDidLoad()
 
         // Do any additional setup after loading the view.
+        
+        if let imageToLoad = selectedImage {
+            imageView.image = UIImage(named: imageToLoad)
+        }
     }
     
 
--- Project1-Storm-Viewer/ViewController.swift
+++ Project1-Storm-Viewer/ViewController.swift
@@ -37,5 +37,12 @@ class ViewController: UITableViewController {
         cell.textLabel?.text = pictures[indexPath.row]
         return cell
     }
+    
+    override func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
+        if let vc = storyboard?.instantiateViewController(withIdentifier: "Detail") as? DetailViewController {
+            vc.selectedImage = pictures[indexPath.row]
+            navigationController?.pushViewController(vc, animated: true)
+        }
+    }
 }
 
