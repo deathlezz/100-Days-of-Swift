edfdb35a35f64db9fcd68cb55accf0347e7e1a64
DEATHLEZZ <78265796+deathlezz@users.noreply.github.com>
2022-06-15 11:43:15 +0100

    Setting up.

---
+++ Project_2/Project2-Guess-the-Flag/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Project2-Guess-the-Flag
+//
+//  Created by deathlezz on 15/06/2022.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
