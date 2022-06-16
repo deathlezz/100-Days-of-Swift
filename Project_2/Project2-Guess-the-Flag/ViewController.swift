7ed3ddc8704e0cf0a14ee2eea998c6d67ad5876b
deathlezz <deathlezz@wp.pl>
2022-06-16 11:52:42 +0100

    Designing your layout

--- Project_2/Project2-Guess-the-Flag/ViewController.swift
+++ Project_2/Project2-Guess-the-Flag/ViewController.swift
@@ -9,9 +9,16 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet var button1: UIButton!
+    @IBOutlet var button2: UIButton!
+    @IBOutlet var button3: UIButton!
+    
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
+        
+        
     }
 
 }
