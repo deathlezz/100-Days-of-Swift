05c48f703a2b2129c4edaf2340e575e2caa8c178
deathlezz <deathlezz@wp.pl>
2022-06-20 11:43:31 +0100

    Challenge.

---
+++ Milestone_Projects_1-3/Milestone_Projects_1-3/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Milestone_Projects_1-3
+//
+//  Created by test on 20/06/2022.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
