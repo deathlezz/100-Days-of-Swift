2fe0ceaca7c226ceee9f467048537f8fe238671e
deathlezz <deathlezz@wp.pl>
2022-06-26 12:10:34 +0100

    Setting up.

---
+++ 05-Project4/Project4-Easy-Browser/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Project4-Easy-Browser
+//
+//  Created by deathlezz on 26/06/2022.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
