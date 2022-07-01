d739cb38a16b288d30bf2cb83e488ead0ac7a63f
deathlezz <deathlezz@wp.pl>
2022-07-01 10:55:41 +0100

    Setting up

---
+++ Project5-Word-Scramble/Project5-Word-Scramble/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Project5-Word-Scramble
+//
+//  Created by deathlezz on 01/07/2022.
+//
+
+import UIKit
+
+class ViewController: UITableViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
