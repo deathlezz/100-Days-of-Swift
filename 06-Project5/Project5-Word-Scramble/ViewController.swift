85d2b71b4d4eaec3ce0401861470fbc475f03cd9
deathlezz <deathlezz@wp.pl>
2022-07-02 12:03:17 +0100

    Pick a word, any word: UIAlertController

--- 06-Project5/Project5-Word-Scramble/ViewController.swift
+++ 06-Project5/Project5-Word-Scramble/ViewController.swift
@@ -16,6 +16,8 @@ class ViewController: UITableViewController {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
         
+        navigationItem.rightBarButtonItem = UIBarButtonItem(barButtonSystemItem: .add, target: self, action: #selector(askForAnswer))
+        
         if let startWordsURL = Bundle.main.url(forResource: "start", withExtension: "txt") {
             if let startWords = try? String(contentsOf: startWordsURL) {
                 allWords = startWords.components(separatedBy: "\n")
@@ -43,7 +45,24 @@ class ViewController: UITableViewController {
         cell.textLabel?.text = usedWords[indexPath.row]
         return cell
     }
+    
+    @objc func askForAnswer() {
+        let ac = UIAlertController(title: "Enter answer", message: nil, preferredStyle: .alert)
+        ac.addTextField()
+        
+        let submitAction = UIAlertAction(title: "Submit", style: .default) {
+            [weak self, weak ac] _ in
+            guard let answer = ac?.textFields?[0].text else { return }
+            self?.submit(answer)
+        }
+        
+        ac.addAction(submitAction)
+        present(ac, animated: true)
+    }
 
-
+    func submit(_ answer: String) {
+        
+    }
+    
 }
 
