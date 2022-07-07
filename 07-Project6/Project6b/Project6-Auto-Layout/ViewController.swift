9b46f2ee495d874173dd9e88121995ef4713e30b
deathlezz <deathlezz@wp.pl>
2022-07-07 12:44:43 +0100

    Auto Layout metrics and priorities: constraints(withVisualFormat:)

--- 07-Project6/Project6b/Project6-Auto-Layout/ViewController.swift
+++ 07-Project6/Project6b/Project6-Auto-Layout/ViewController.swift
@@ -55,7 +55,9 @@ class ViewController: UIViewController {
             view.addConstraints(NSLayoutConstraint.constraints(withVisualFormat: "H:|[\(label)]|", options: [], metrics: nil, views: viewsDictionary))
         }
         
-        view.addConstraints(NSLayoutConstraint.constraints(withVisualFormat: "V:|[label1]-[label2]-[label3]-[label4]-[label5]", options: [], metrics: nil, views: viewsDictionary))
+        let metrics = ["labelHeight": 88]
+        
+        view.addConstraints(NSLayoutConstraint.constraints(withVisualFormat: "V:|[label1(labelHeight@999)]-[label2(label1)]-[label3(label1)]-[label4(label1)]-[label5(label1)]-(>=10)-|", options: [], metrics: metrics, views: viewsDictionary))
     }
 
 
