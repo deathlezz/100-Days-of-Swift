8365dab7686b83494016f70bf41875a09df76c6d
deathlezz <deathlezz@wp.pl>
2022-07-07 11:06:49 +0100

    Setting up

---
+++ Project6-Auto-Layout/Project6-Auto-Layout/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Project6-Auto-Layout
+//
+//  Created by deathlezz on 07/07/2022.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
