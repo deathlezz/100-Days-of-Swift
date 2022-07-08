38bde83707085cce2b424b0f2841f57c667357c4
deathlezz <deathlezz@wp.pl>
2022-07-08 11:07:46 +0100

    Challenge

--- 07-Project6/Project6b/Project6-Auto-Layout/ViewController.swift
+++ 07-Project6/Project6b/Project6-Auto-Layout/ViewController.swift
@@ -62,8 +62,12 @@ class ViewController: UIViewController {
         var previous: UILabel?
 
         for label in [label1, label2, label3, label4, label5] {
-            label.widthAnchor.constraint(equalTo: view.widthAnchor).isActive = true
-            label.heightAnchor.constraint(equalToConstant: 88).isActive = true
+            // label.widthAnchor.constraint(equalTo: view.widthAnchor).isActive = true
+            label.leadingAnchor.constraint(equalTo: view.safeAreaLayoutGuide.leadingAnchor).isActive = true
+            label.trailingAnchor.constraint(equalTo: view.safeAreaLayoutGuide.trailingAnchor).isActive = true
+            
+            // label.heightAnchor.constraint(equalToConstant: 88).isActive = true
+            label.heightAnchor.constraint(equalTo: view.safeAreaLayoutGuide.heightAnchor, multiplier: 0.2, constant: -10).isActive = true
             
             if let previous = previous {
                 label.topAnchor.constraint(equalTo: previous.bottomAnchor, constant: 10).isActive = true
