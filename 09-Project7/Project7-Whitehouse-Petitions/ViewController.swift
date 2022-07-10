a9fe0b8d9ac8958f0bd8608550167fb887ab7eb6
deathlezz <deathlezz@wp.pl>
2022-07-10 11:40:43 +0100

    Creating the basic UI: UITabBarController

--- 09-Project7/Project7-Whitehouse-Petitions/ViewController.swift
+++ 09-Project7/Project7-Whitehouse-Petitions/ViewController.swift
@@ -7,11 +7,24 @@
 
 import UIKit
 
-class ViewController: UIViewController {
+class ViewController: UITableViewController {
+    
+    var petitions = [String]()
 
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
+    }
+    
+    override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return petitions.count
+    }
+    
+    override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = tableView.dequeueReusableCell(withIdentifier: "Cell", for: indexPath)
+        cell.textLabel?.text = "Title goes here"
+        cell.detailTextLabel?.text = "Subtitle goes here"
+        return cell
     }
 
 
