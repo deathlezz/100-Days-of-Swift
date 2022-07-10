403e80a72d44ffe559a1251005e38a8664d10562
deathlezz <deathlezz@wp.pl>
2022-07-10 10:24:07 +0100

    Setting up

---
+++ Project7-Whitehouse-Petitions/Project7-Whitehouse-Petitions/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Project7-Whitehouse-Petitions
+//
+//  Created by deathlezz on 10/07/2022.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
