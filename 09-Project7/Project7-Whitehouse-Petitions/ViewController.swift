c840992afe4c2801afa47034bd18b639c3136c6e
deathlezz <deathlezz@wp.pl>
2022-07-11 23:13:51 +0100

    Finishing touches: didFinishLaunchingWithOptions

--- 09-Project7/Project7-Whitehouse-Petitions/ViewController.swift
+++ 09-Project7/Project7-Whitehouse-Petitions/ViewController.swift
@@ -15,14 +15,29 @@ class ViewController: UITableViewController {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
         
-        let urlString = "https://www.hackingwithswift.com/samples/petitions-1.json"
+        let urlString: String
+        
+        if navigationController?.tabBarItem.tag == 0 {
+            urlString = "https://www.hackingwithswift.com/samples/petitions-1.json"
+        } else {
+            urlString = "https://www.hackingwithswift.com/samples/petitions-2.json"
+        }
+        
         
         if let url = URL(string: urlString) {
             if let data = try? Data(contentsOf: url) {
                 // we are OK to parse
                 parse(json: data)
+                return
             }
         }
+        showError()
+    }
+    
+    func showError() {
+        let ac = UIAlertController(title: "Loading error", message: "There was a problem loading the feed; please check your connection and try again.", preferredStyle: .alert)
+        ac.addAction(UIAlertAction(title: "OK", style: .default))
+        present(ac, animated: true)
     }
     
     func parse(json: Data) {
