61b4842c9756ed91de6d9b39f7a9bcc4df2ce2a8
deathlezz <deathlezz@wp.pl>
2022-07-13 11:16:47 +0100

    Wrap up

---
+++ Project8-7-Swifty-Words/Project8-7-Swifty-Words/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Project8-7-Swifty-Words
+//
+//  Created by deathlezz on 13/07/2022.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
