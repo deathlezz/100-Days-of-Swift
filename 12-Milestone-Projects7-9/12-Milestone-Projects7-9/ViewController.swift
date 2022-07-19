80b244ff746e8e9397bd1f45bb41f26d3b037934
deathlezz <deathlezz@wp.pl>
2022-07-19 11:11:38 +0100

    Setting up

---
+++ 12-Milestone-Projects7-9/12-Milestone-Projects7-9/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  12-Milestone-Projects7-9
+//
+//  Created by deathlezz on 19/07/2022.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
