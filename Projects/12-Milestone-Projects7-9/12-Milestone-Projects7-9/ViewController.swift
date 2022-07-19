0ac9db3bd312d44ca5b4b33d625b5bb8bf268979
deathlezz <deathlezz@wp.pl>
2022-07-19 13:29:18 +0100

    Setting up the UI

--- Projects/12-Milestone-Projects7-9/12-Milestone-Projects7-9/ViewController.swift
+++ Projects/12-Milestone-Projects7-9/12-Milestone-Projects7-9/ViewController.swift
@@ -8,10 +8,32 @@
 import UIKit
 
 class ViewController: UIViewController {
-
+    
+    @IBOutlet var imageView: UIImageView!
+    @IBOutlet var wordLabel: UILabel!
+    @IBOutlet var row1: UIView!
+    @IBOutlet var row2: UIView!
+    @IBOutlet var row3: UIView!
+    @IBOutlet var row4: UIView!
+    
+    var currentWord = ""
+    
+    var allWorlds = [String]()
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
+        
+        title = "Hangman"
+        imageView.image = UIImage(named: "hangman0")
+        
+        if let wordsURL = Bundle.main.url(forResource: "english", withExtension: "txt") {
+            if let words = try? String(contentsOf: wordsURL) {
+                allWorlds = words.components(separatedBy: "\n")
+            }
+        }
+        currentWord = allWorlds.randomElement() ?? "silkworm"
+        wordLabel.text = String(repeating: "_ ", count: currentWord.count).trimmingCharacters(in: .whitespacesAndNewlines)
     }
 
 
