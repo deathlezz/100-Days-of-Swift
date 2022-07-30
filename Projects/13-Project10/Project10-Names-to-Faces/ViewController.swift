f009720d02871015623317fd7cd8ee6acdd102b3
deathlezz <deathlezz@wp.pl>
2022-07-30 20:57:24 +0100

    UICollectionView data sources

--- Projects/13-Project10/Project10-Names-to-Faces/ViewController.swift
+++ Projects/13-Project10/Project10-Names-to-Faces/ViewController.swift
@@ -13,6 +13,17 @@ class ViewController: UICollectionViewController {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
     }
+    
+    override func collectionView(_ collectionView: UICollectionView, numberOfItemsInSection section: Int) -> Int {
+        return 10
+    }
+    
+    override func collectionView(_ collectionView: UICollectionView, cellForItemAt indexPath: IndexPath) -> UICollectionViewCell {
+        guard let cell = collectionView.dequeueReusableCell(withReuseIdentifier: "Person", for: indexPath) as? PersonCell else {
+            fatalError("Unable to dequeue PersonCell.")
+        }
+        return cell
+    }
 
 
 }
