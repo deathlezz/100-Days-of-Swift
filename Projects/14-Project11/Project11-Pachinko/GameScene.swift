f2f8e476e3fd6397f6766eda4594f5d48c89f323
deathlezz <deathlezz@wp.pl>
2022-08-06 22:10:04 +0100

    Bouncing balls: circleOfRadius

--- Projects/14-Project11/Project11-Pachinko/GameScene.swift
+++ Projects/14-Project11/Project11-Pachinko/GameScene.swift
@@ -17,16 +17,31 @@ class GameScene: SKScene {
         addChild(background)
         
         physicsBody = SKPhysicsBody(edgeLoopFrom: frame)
+        
+        makeBouncer(at: CGPoint(x: 0, y: 0))
+        makeBouncer(at: CGPoint(x: 256, y: 0))
+        makeBouncer(at: CGPoint(x: 512, y: 0))
+        makeBouncer(at: CGPoint(x: 768, y: 0))
+        makeBouncer(at: CGPoint(x: 1024, y: 0))
     }
     
     override func touchesBegan(_ touches: Set<UITouch>, with event: UIEvent?) {
         guard let touch = touches.first else { return }
         let location = touch.location(in: self)
         
-        let box = SKSpriteNode(color: .red, size: CGSize(width: 64, height: 64))
-        box.physicsBody = SKPhysicsBody(rectangleOf: CGSize(width: 64, height: 64))
-        box.position = location
-        addChild(box)
+        let ball = SKSpriteNode(imageNamed: "ballRed")
+        ball.physicsBody = SKPhysicsBody(circleOfRadius: ball.size.width / 2)
+        ball.physicsBody?.restitution = 0.4
+        ball.position = location
+        addChild(ball)
+    }
+    
+    func makeBouncer(at position: CGPoint) {
+        let bouncer = SKSpriteNode(imageNamed: "bouncer")
+        bouncer.position = position
+        bouncer.physicsBody = SKPhysicsBody(circleOfRadius: bouncer.size.width / 2)
+        bouncer.physicsBody?.isDynamic = false
+        addChild(bouncer)
     }
 
 }
