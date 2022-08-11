e45a5a78f768950e2fef293e11a393a12f38c792
deathlezz <deathlezz@wp.pl>
2022-08-11 22:28:48 +0100

    Reading and writing basics: UserDefaults

--- Projects/15-Project12/Test/Test/ViewController.swift
+++ Projects/15-Project12/Test/Test/ViewController.swift
@@ -12,6 +12,26 @@ class ViewController: UIViewController {
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
+        
+        let defaults = UserDefaults.standard
+        
+        defaults.set(25, forKey: "Age")
+        defaults.set(true, forKey: "UseFaceID")
+        defaults.set(CGFloat.pi, forKey: "Pi")
+        defaults.set("Paul Hudson", forKey: "Name")
+        defaults.set(Date(), forKey: "LastRun")
+        
+        let array = ["Hello", "World"]
+        defaults.set(array, forKey: "SavedArray")
+        
+        let dict = ["Name": "Paul", "Country": "UK"]
+        defaults.set(dict, forKey: "SavedDictionary")
+        
+        let savedInteger = defaults.integer(forKey: "Age")
+        let savedBoolean = defaults.bool(forKey: "UseFaceID")
+        
+        let savedArray = defaults.object(forKey: "SavedArray") as? [String] ?? [String]()
+        let savedDictionary = defaults.object(forKey: "SavedDictionary") as? [String: String] ?? [String: String]()
     }
 
 
