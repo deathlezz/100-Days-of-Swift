7adf89bc94014a4dc0c26fabafbbc59146c514a3
deathlezz <deathlezz@wp.pl>
2022-08-14 13:20:38 +0100

    Challenge 2

---
+++ Projects/15-Project12/Challenge2/Project2-Guess-the-Flag/ViewController.swift
@@ -0,0 +1,96 @@
+//
+//  ViewController.swift
+//  Project2-Guess-the-Flag
+//
+//  Created by deathlezz on 15/06/2022.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    @IBOutlet var button1: UIButton!
+    @IBOutlet var button2: UIButton!
+    @IBOutlet var button3: UIButton!
+    
+    var topScore = 0
+    
+    var countries = [String]()
+    var score = 0
+    var correctAnswerIndex = 0
+    var answeredQuestions = 0
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+        
+        countries += ["estonia", "france", "germany", "ireland", "italy", "monaco", "nigeria", "poland", "russia", "spain", "uk", "us"]
+        
+        button1.layer.borderWidth = 1
+        button2.layer.borderWidth = 1
+        button3.layer.borderWidth = 1
+        
+        button1.layer.borderColor = UIColor.lightGray.cgColor
+        button2.layer.borderColor = UIColor.lightGray.cgColor
+        button3.layer.borderColor = UIColor.lightGray.cgColor
+        
+        navigationItem.rightBarButtonItem = UIBarButtonItem(title: "Score", style: .plain, target: self, action: #selector(scoreTapped))
+        
+        askQuestion()
+        
+        let defaults = UserDefaults.standard
+        topScore = defaults.integer(forKey: "TopScore")
+    }
+
+    func askQuestion(action: UIAlertAction! = nil) {
+        countries.shuffle()
+        correctAnswerIndex = Int.random(in: 0...2)
+        
+        button1.setImage(UIImage(named: countries[0]), for: .normal)
+        button2.setImage(UIImage(named: countries[1]), for: .normal)
+        button3.setImage(UIImage(named: countries[2]), for: .normal)
+        
+        title = "\(countries[correctAnswerIndex].uppercased()) | Score: \(score)"
+    }
+    
+    @IBAction func buttonTapped(_ sender: UIButton) {
+        var title: String
+        
+        if sender.tag == correctAnswerIndex {
+            title = "Correct"
+            score += 1
+        } else {
+            title = "Wrong! That’s the flag of \(countries[sender.tag].uppercased())"
+            score -= 1
+        }
+        
+        answeredQuestions += 1
+        
+        if answeredQuestions < 10 {
+            let ac = UIAlertController(title: title, message: "Your score is \(score)", preferredStyle: .alert)
+            ac.addAction(UIAlertAction(title: "Continue", style: .default, handler: askQuestion))
+            present(ac, animated: true)
+
+        } else {
+            let ac = UIAlertController(title: "Game Over", message: "Your final score is \(score)", preferredStyle: .alert)
+            ac.addAction(UIAlertAction(title: "New Game", style: .default, handler: askQuestion))
+            present(ac, animated: true)
+            
+            score = 0
+            answeredQuestions = 0
+        }
+    }
+    
+    @objc func scoreTapped() {
+        let ac = UIAlertController(title: "Score", message: "Your score is \(score)\n Your best score is \(topScore)", preferredStyle: .actionSheet)
+        ac.addAction(UIAlertAction(title: "OK", style: .default))
+        present(ac, animated: true)
+    }
+    
+    func saveTopScore() {
+        let defaults = UserDefaults.standard
+        defaults.set(topScore, forKey: "TopScore")
+    }
+    
+}
+
