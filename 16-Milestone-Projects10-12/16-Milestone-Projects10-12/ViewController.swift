40c55f845db95b478373c9f381056a67794ae9be
deathlezz <deathlezz@wp.pl>
2022-08-17 17:38:13 +0100

    Added "Milestone: Projects 10-12"

---
+++ 16-Milestone-Projects10-12/16-Milestone-Projects10-12/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  16-Milestone-Projects10-12
+//
+//  Created by deathlezz on 17/08/2022.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
