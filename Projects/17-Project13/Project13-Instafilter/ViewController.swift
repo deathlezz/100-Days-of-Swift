67d6a02bd82138fd1b21b0447625405b13d5db52
deathlezz <deathlezz@wp.pl>
2022-08-24 21:44:00 +0100

    Designing the interface

--- Projects/17-Project13/Project13-Instafilter/ViewController.swift
+++ Projects/17-Project13/Project13-Instafilter/ViewController.swift
@@ -8,12 +8,28 @@
 import UIKit
 
 class ViewController: UIViewController {
-
+    
+    @IBOutlet var imageView: UIImageView!
+    @IBOutlet var intensity: UISlider!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
+        
     }
-
+    
+    @IBAction func intensity(_ sender: Any) {
+        
+    }
+    
+    @IBAction func save(_ sender: Any) {
+        
+    }
+    
+    @IBAction func intensityChanged(_ sender: Any) {
+        
+    }
+    
 
 }
 
