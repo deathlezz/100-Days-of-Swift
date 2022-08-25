ac815da05981d9143ad4feceb3cf895c2ae5f7e0
deathlezz <deathlezz@wp.pl>
2022-08-25 22:40:56 +0100

    Saving to the iOS photo library

--- Projects/17-Project13/Project13-Instafilter/ViewController.swift
+++ Projects/17-Project13/Project13-Instafilter/ViewController.swift
@@ -64,7 +64,6 @@ class ViewController: UIViewController, UIImagePickerControllerDelegate, UINavig
         }
         
         present(ac, animated: true)
-        
     }
     
     func setFilter(action: UIAlertAction) {
@@ -80,7 +79,9 @@ class ViewController: UIViewController, UIImagePickerControllerDelegate, UINavig
     }
     
     @IBAction func save(_ sender: Any) {
+        guard let image = imageView.image else { return }
         
+        UIImageWriteToSavedPhotosAlbum(image, self, #selector(image(_:didFinishSavingWithError:contextInfo:)), nil)
     }
     
     @IBAction func intensityChanged(_ sender: Any) {
@@ -110,6 +111,17 @@ class ViewController: UIViewController, UIImagePickerControllerDelegate, UINavig
         if let cgImage = context.createCGImage(outputImage, from: outputImage.extent) {
             let processedImage = UIImage(cgImage: cgImage)
             imageView.image = processedImage
+        }
+    }
+    
+    @objc func image(_ image: UIImage, didFinishSavingWithError error: Error?, contextInfo: UnsafeRawPointer) {
+        if let error = error {
+            let ac = UIAlertController(title: "Save error", message: error.localizedDescription, preferredStyle: .alert)
+            ac.addAction(UIAlertAction(title: "OK", style: .default))
+        } else {
+            let ac = UIAlertController(title: "Saved!", message: "Your altered image has been saved to your photos.", preferredStyle: .alert)
+            ac.addAction(UIAlertAction(title: "OK", style: .default))
+            present(ac, animated: true)
         }
     }
 
