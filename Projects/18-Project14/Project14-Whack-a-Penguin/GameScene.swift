7c27b8a929ba795400e07761ef1efafcd80cc50b
deathlezz <deathlezz@wp.pl>
2022-08-27 13:17:30 +0100

    Penguin, show thyself: SKAction moveBy(x:y:duration:)

--- Projects/18-Project14/Project14-Whack-a-Penguin/GameScene.swift
+++ Projects/18-Project14/Project14-Whack-a-Penguin/GameScene.swift
@@ -12,6 +12,8 @@ class GameScene: SKScene {
     var slots = [WhackSlot]()
     var gameScore: SKLabelNode!
     
+    var popupTime = 0.85
+    
     var score = 0 {
         didSet {
             gameScore.text = "Score: \(score)"
@@ -37,6 +39,10 @@ class GameScene: SKScene {
         for i in 0..<4 { createSlot(at: CGPoint(x: 180 + (i * 170), y: 320)) }
         for i in 0..<5 { createSlot(at: CGPoint(x: 100 + (i * 170), y: 230)) }
         for i in 0..<4 { createSlot(at: CGPoint(x: 180 + (i * 170), y: 140)) }
+        
+        DispatchQueue.main.asyncAfter(deadline: .now() + 1) { [weak self] in
+            self?.createEnemy()
+        }
     }
 
     override func touchesBegan(_ touches: Set<UITouch>, with event: UIEvent?) {
@@ -48,5 +54,26 @@ class GameScene: SKScene {
         slot.configure(at: position)
         addChild(slot)
         slots.append(slot)
+    }
+    
+    func createEnemy() {
+        popupTime *= 0.991
+        
+        slots.shuffle()
+        slots[0].show(hideTime: popupTime)
+        
+        if Int.random(in: 0...12) > 4 { slots[1].show(hideTime: popupTime) }
+        if Int.random(in: 0...12) > 8 { slots[2].show(hideTime: popupTime) }
+        if Int.random(in: 0...12) > 10 { slots[3].show(hideTime: popupTime) }
+        if Int.random(in: 0...12) > 11 { slots[4].show(hideTime: popupTime) }
+        
+        let minDelay = popupTime / 2.0
+        let maxDelay = popupTime * 2
+        
+        let delay = Double.random(in: minDelay...maxDelay)
+        
+        DispatchQueue.main.asyncAfter(deadline: .now() + delay) { [weak self] in
+            self?.createEnemy()
+        }
     }
 }
--- Projects/18-Project14/Project14-Whack-a-Penguin/WhackSlot.swift
+++ Projects/18-Project14/Project14-Whack-a-Penguin/WhackSlot.swift
@@ -11,6 +11,9 @@ import UIKit
 class WhackSlot: SKNode {
     var charNode: SKSpriteNode!
     
+    var isVisible = false
+    var isHit = false
+    
     func configure(at position: CGPoint) {
         self.position = position
         
@@ -28,5 +31,33 @@ class WhackSlot: SKNode {
         
         cropNode.addChild(charNode)
         addChild(cropNode)
+    }
+    
+    func show(hideTime: Double) {
+        if isVisible { return }
+        
+        charNode.run(SKAction.moveBy(x: 0, y: 80, duration: 0.05))
+        
+        isVisible = true
+        isHit = false
+        
+        if Int.random(in: 0...2) == 0 {
+            charNode.texture = SKTexture(imageNamed: "penguinGood")
+            charNode.name = "charFriend"
+        } else {
+            charNode.texture = SKTexture(imageNamed: "penguinEvil")
+            charNode.name = "charEnemy"
+        }
+        
+        DispatchQueue.main.asyncAfter(deadline: .now() + (hideTime * 3.5)) { [weak self] in
+            self?.hide()
+        }
+    }
+    
+    func hide() {
+        if !isVisible { return }
+        
+        charNode.run(SKAction.moveBy(x: 0, y: -80, duration: 0.05))
+        isVisible = false
     }
 }
