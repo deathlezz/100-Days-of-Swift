0865070ccee379b29af2847212cb23027348707c
deathlezz <deathlezz@wp.pl>
2022-08-31 17:03:03 +0100

    Switch, case, animate: animate(withDuration:)

--- Projects/19-Project15/Project15-Animation/ViewController.swift
+++ Projects/19-Project15/Project15-Animation/ViewController.swift
@@ -21,7 +21,21 @@ class ViewController: UIViewController {
         view.addSubview(imageView)
     }
 
-    @IBAction func tapped(_ sender: Any) {
+    @IBAction func tapped(_ sender: UIButton) {
+        
+        sender.isHidden = true
+        
+        UIView.animate(withDuration: 1, delay: 0, options: [], animations: {
+            switch self.currentAnimation {
+            case 0:
+                break
+            default:
+                break
+            }
+        }) { finished in
+            sender.isHidden = false
+        }
+        
         currentAnimation += 1
         
         if currentAnimation > 7 {
