b15a7341e7e45a0f608c16cb76b007cae74cef52
deathlezz <deathlezz@wp.pl>
2022-08-31 17:24:29 +0100

    Transform: CGAffineTransform

--- Projects/19-Project15/Project15-Animation/ViewController.swift
+++ Projects/19-Project15/Project15-Animation/ViewController.swift
@@ -25,10 +25,28 @@ class ViewController: UIViewController {
         
         sender.isHidden = true
         
-        UIView.animate(withDuration: 1, delay: 0, options: [], animations: {
+//        UIView.animate(withDuration: 1, delay: 0, options: [], animations: {
+        UIView.animate(withDuration: 1, delay: 0, usingSpringWithDamping: 0.5, initialSpringVelocity: 5, options: [], animations: {
             switch self.currentAnimation {
             case 0:
+                self.imageView.transform = CGAffineTransform(scaleX: 2, y: 2)
                 break
+            case 1:
+                self.imageView.transform = .identity
+            case 2:
+                self.imageView.transform = CGAffineTransform(translationX: -256, y: -256)
+            case 3:
+                self.imageView.transform = .identity
+            case 4:
+                self.imageView.transform = CGAffineTransform(rotationAngle: .pi)
+            case 5:
+                self.imageView.transform = .identity
+            case 6:
+                self.imageView.alpha = 0.2
+                self.imageView.backgroundColor = .green
+            case 7:
+                self.imageView.alpha = 1
+                self.imageView.backgroundColor = .clear
             default:
                 break
             }
