1aa952bc872d48a4bdb13b98a831ea7da0405db1
deathlezz <deathlezz@wp.pl>
2022-09-07 11:26:51 +0100

    Up and running with MapKit

--- Projects/21-Project16/Project16-Capital-Cities/ViewController.swift
+++ Projects/21-Project16/Project16-Capital-Cities/ViewController.swift
@@ -5,13 +5,28 @@
 //  Created by deathlezz on 07/09/2022.
 //
 
+import MapKit
 import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet var mapView: MKMapView!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
+        
+        let london = Capital(title: "London", coordinate: CLLocationCoordinate2D(latitude: 51.507222, longitude: -0.1275), info: "Home to the 2012 Summer Olimpics.")
+        
+        let oslo = Capital(title: "Oslo", coordinate: CLLocationCoordinate2D(latitude: 59.95, longitude: 10.75), info: "It was founded over a 1000 years ago.")
+        
+        let paris =  Capital(title: "Paris", coordinate: CLLocationCoordinate2D(latitude: 48.8567, longitude: 2.3508), info: "Often called the City of Light.")
+        
+        let rome = Capital(title: "Rome", coordinate: CLLocationCoordinate2D(latitude: 41.9, longitude: 12.5), info: "Has a whole country inside it.")
+        
+        let washington = Capital(title: "Washington DC", coordinate: CLLocationCoordinate2D(latitude: 38.895111, longitude: -77.036667), info: "Named after George himself.")
+        
+        mapView.addAnnotations([london, oslo, paris, rome, washington])
     }
 
 
