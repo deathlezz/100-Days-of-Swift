a5684338bd61e62f90537a0c4cc1a711e868c076
deathlezz <deathlezz@wp.pl>
2022-09-07 10:48:25 +0100

    Setting up

---
+++ Project16-Capital-Cities/Project16-Capital-Cities/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Project16-Capital-Cities
+//
+//  Created by deathlezz on 07/09/2022.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
