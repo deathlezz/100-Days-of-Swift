4c18943857afd0471e4c8993e36887b25dd2414c
deathlezz <deathlezz@wp.pl>
2022-09-09 11:16:10 +0100

    Setting up

---
+++ Project17-Space-Race/Project17-Space-Race/GameScene.swift
@@ -0,0 +1,19 @@
+//
+//  GameScene.swift
+//  Project17-Space-Race
+//
+//  Created by deathlezz on 09/09/2022.
+//
+
+import SpriteKit
+
+class GameScene: SKScene {
+    
+    override func didMove(to view: SKView) {
+        
+    }
+
+    override func update(_ currentTime: TimeInterval) {
+        // Called before each frame is rendered
+    }
+}
