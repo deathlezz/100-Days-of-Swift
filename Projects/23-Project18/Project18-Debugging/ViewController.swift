4a29ae8683f9f47d155cb9ff1ca6a821b7e43cb2
deathlezz <deathlezz@wp.pl>
2022-09-11 17:23:38 +0100

    Debugging with breakpoints

--- Projects/23-Project18/Project18-Debugging/ViewController.swift
+++ Projects/23-Project18/Project18-Debugging/ViewController.swift
@@ -13,10 +13,14 @@ class ViewController: UIViewController {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
         
-        print(1, 2, 3, 4, 5, separator: "-", terminator: "")
-        print("some message")
+//        print(1, 2, 3, 4, 5, separator: "-", terminator: "")
+//        print("some message")
+//
+//        assert(myReallySlowMethod() == true, "The slow method returned false, which is a bad thing.")
         
-        assert(myReallySlowMethod() == true, "The slow method returned false, which is a bad thing.")
+        for i in 1...100 {
+            print("Got number \(i)")
+        }
     }
 
     func myReallySlowMethod() -> Bool {
