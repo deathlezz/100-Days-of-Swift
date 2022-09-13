82a3954d8d5ff72830a4b9c8047fa3ce7a4088dc
deathlezz <deathlezz@wp.pl>
2022-09-13 11:01:57 +0100

    Wrap up

---
+++ Projects/23-Project18/Challenge1/Project1-Storm-Viewer/ViewController.swift
@@ -0,0 +1,64 @@
+//
+//  ViewController.swift
+//  Project1-Storm-Viewer
+//
+//  Created by deathlezz on 11/06/2022.
+//
+
+import UIKit
+
+class ViewController: UITableViewController {
+    
+    var pictures = [String]()
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+        
+        title = "Storm Viewer"
+        navigationController?.navigationBar.prefersLargeTitles = true
+        
+        navigationItem.rightBarButtonItem = UIBarButtonItem(barButtonSystemItem: .action, target: self, action: #selector(recommendApp))
+        
+        let fm = FileManager.default
+        let path = Bundle.main.resourcePath!
+        let items = try! fm.contentsOfDirectory(atPath: path)
+        
+        for item in items {
+            if item.hasPrefix("nssl") {
+                // this is a picture to load!
+                pictures.append(item)
+            }
+        }
+        pictures.sort()
+    }
+    
+    override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return pictures.count
+    }
+    
+    override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = tableView.dequeueReusableCell(withIdentifier: "Picture", for: indexPath)
+        cell.textLabel?.text = pictures[indexPath.row]
+        return cell
+    }
+    
+    // challenge 1
+    override func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
+        if let vc = storyboard?.instantiateViewController(withIdentifier: "Bad") as? DetailViewController {
+            vc.selectedImage = pictures[indexPath.row]
+            vc.selectedImageNumber = indexPath.row + 1
+            vc.totalPictures = pictures.count
+            navigationController?.pushViewController(vc, animated: true)
+        }
+    }
+    
+    @objc func recommendApp() {
+        let shareURL = "https://yourAppURL.com"
+        
+        let vc = UIActivityViewController(activityItems: [shareURL], applicationActivities: [])
+        vc.popoverPresentationController?.barButtonItem = navigationItem.rightBarButtonItem
+        present(vc, animated: true)
+    }
+}
+
---
+++ Projects/23-Project18/Challenge2/Project1-Storm-Viewer/DetailViewController.swift
@@ -0,0 +1,54 @@
+//
+//  DetailViewController.swift
+//  Project1-Storm-Viewer
+//
+//  Created by deathlezz on 12/06/2022.
+//
+
+import UIKit
+
+class DetailViewController: UIViewController {
+    
+    @IBOutlet var imageView: UIImageView!
+    
+    var selectedImage: String?
+    var selectedImageNumber: Int?
+    var totalPictures: Int?
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view..
+        
+        // challenge 2
+        assert(selectedImage != nil, "selectedImage has no value.")
+        
+        title = "\(selectedImageNumber!) of \(totalPictures!)"
+        navigationItem.largeTitleDisplayMode = .never
+        
+        if let imageToLoad = selectedImage {
+            imageView.image = UIImage(named: imageToLoad)
+        }
+    }
+    
+    override func viewWillAppear(_ animated: Bool) {
+        super.viewWillAppear(animated)
+        navigationController?.hidesBarsOnTap = true
+    }
+    
+    override func viewWillDisappear(_ animated: Bool) {
+        super.viewWillDisappear(animated)
+        navigationController?.hidesBarsOnTap = false
+    }
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
