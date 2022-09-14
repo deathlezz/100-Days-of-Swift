70fb4cf70c07362487114efc252fb429f07f1fd6
deathlezz <deathlezz@wp.pl>
2022-09-14 12:15:11 +0100

    Setting up

---
+++ Milestone-Projects16-18/Milestone-Projects16-18/GameScene.swift
@@ -0,0 +1,20 @@
+//
+//  GameScene.swift
+//  Milestone-Projects16-18
+//
+//  Created by deathlezz on 14/09/2022.
+//
+
+import SpriteKit
+
+class GameScene: SKScene {
+    
+    override func didMove(to view: SKView) {
+        
+    }
+    
+    override func update(_ currentTime: TimeInterval) {
+        // Called before each frame is rendered
+        
+    }
+}
