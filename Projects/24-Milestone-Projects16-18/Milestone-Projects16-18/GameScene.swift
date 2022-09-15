0e50addb0755e6f2c234d5f9f8b1decf3a5a0dc1
deathlezz <deathlezz@wp.pl>
2022-09-15 12:50:46 +0100

    Added assets

--- Projects/24-Milestone-Projects16-18/Milestone-Projects16-18/GameScene.swift
+++ Projects/24-Milestone-Projects16-18/Milestone-Projects16-18/GameScene.swift
@@ -9,7 +9,22 @@ import SpriteKit
 
 class GameScene: SKScene {
     
+    var background: SKSpriteNode!
+    var grass1: SKSpriteNode!
+    var grass2: SKSpriteNode!
+    var grass3: SKSpriteNode!
+    
     override func didMove(to view: SKView) {
+        
+        background = SKSpriteNode(imageNamed: "background")
+        background.blendMode = .replace
+        background.position = CGPoint(x: 512, y: 384)
+        background.zPosition = -1
+        addChild(background)
+        
+        grass1 = SKSpriteNode(imageNamed: "grass1")
+        grass1.position = CGPoint(x: 512, y: 384)
+        addChild(grass1)
         
     }
     
