a4644bf0d52a2308a3ae7e8fd8cf16b8c950afbf
deathlezz <deathlezz@wp.pl>
2022-09-20 11:03:06 +0100

    Setting up

---
+++ Project19-JavaScript-Injection/Project19-JavaScript-Injection/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Project19-JavaScript-Injection
+//
+//  Created by deathlezz on 20/09/2022.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
