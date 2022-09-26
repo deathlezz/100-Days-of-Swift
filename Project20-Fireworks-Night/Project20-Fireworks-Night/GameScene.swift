a678c47b4951121d57667b6992f7f85dad6bba62
deathlezz <deathlezz@wp.pl>
2022-09-26 11:53:37 +0100

    Setting up

---
+++ Project20-Fireworks-Night/Project20-Fireworks-Night/GameScene.swift
@@ -0,0 +1,16 @@
+//
+//  GameScene.swift
+//  Project20-Fireworks-Night
+//
+//  Created by deathlezz on 26/09/2022.
+//
+
+import SpriteKit
+
+class GameScene: SKScene {
+    
+    override func didMove(to view: SKView) {
+        
+    }
+
+}
