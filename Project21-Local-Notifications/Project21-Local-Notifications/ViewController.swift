3daf067455719cd4a5adc3a87ed0ff6ba238ed27
deathlezz <deathlezz@wp.pl>
2022-09-28 10:36:15 +0100

    Setting up

---
+++ Project21-Local-Notifications/Project21-Local-Notifications/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Project21-Local-Notifications
+//
+//  Created by deathlezz on 28/09/2022.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
