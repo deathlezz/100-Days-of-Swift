15998e9dd17eb2e5f73b61390bb5953e2329b7c1
deathlezz <deathlezz@wp.pl>
2022-10-01 01:08:37 +0100

    Setting up

---
+++ Milestone-Projects16-18/Milestone-Projects16-18/DetailViewController.swift
@@ -0,0 +1,20 @@
+//
+//  DetailViewController.swift
+//  Milestone-Projects16-18
+//
+//  Created by deathlezz on 01/10/2022.
+//
+
+import UIKit
+
+class DetailViewController: UIViewController {
+    
+    @IBOutlet var textView: UITextView!
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+}
