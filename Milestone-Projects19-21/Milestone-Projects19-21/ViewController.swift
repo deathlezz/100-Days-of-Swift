31b3778ac068d0da68eb75a57892da4161337da9
deathlezz <deathlezz@wp.pl>
2022-10-01 01:21:38 +0100

    Setting up

---
+++ Milestone-Projects19-21/Milestone-Projects19-21/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Milestone-Projects19-21
+//
+//  Created by deathlezz on 01/10/2022.
+//
+
+import UIKit
+
+class ViewController: UITableViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
