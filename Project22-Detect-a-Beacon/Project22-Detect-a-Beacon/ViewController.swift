12cfac90fa23ace3b74c872113a4484e93904005
deathlezz <deathlezz@wp.pl>
2022-10-04 12:00:44 +0100

    Setting up

---
+++ Project22-Detect-a-Beacon/Project22-Detect-a-Beacon/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Project22-Detect-a-Beacon
+//
+//  Created by deathlezz on 04/10/2022.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
