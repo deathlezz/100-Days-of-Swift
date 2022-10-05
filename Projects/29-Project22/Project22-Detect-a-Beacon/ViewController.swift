e16c4c0f19ea53424276574d149613cb70d19ea3
deathlezz <deathlezz@wp.pl>
2022-10-05 12:10:29 +0100

    Wrap up

--- Projects/29-Project22/Project22-Detect-a-Beacon/ViewController.swift
+++ Projects/29-Project22/Project22-Detect-a-Beacon/ViewController.swift
@@ -10,8 +10,15 @@ import CoreLocation
 
 class ViewController: UIViewController, CLLocationManagerDelegate {
     
+    var beaconDetected = true
+    
     @IBOutlet var distanceReading: UILabel!
     var locationManager: CLLocationManager?
+    
+    @IBOutlet var nameLabel: UILabel!
+    @IBOutlet var circleView: UIView!
+    
+    var currentUUID: UUID?
     
     override func viewDidLoad() {
         super.viewDidLoad()
@@ -21,6 +28,12 @@ class ViewController: UIViewController, CLLocationManagerDelegate {
         locationManager?.delegate = self
         locationManager?.requestAlwaysAuthorization()
         view.backgroundColor = .gray
+        
+        nameLabel.text = "NO BEACON FOUND"
+        
+        // Challenge 3
+        circleView.layer.cornerRadius = 128
+        circleView.transform = CGAffineTransform(scaleX: 0.001, y: 0.001)
     }
     
     func locationManager(_ manager: CLLocationManager, didChangeAuthorization status: CLAuthorizationStatus) {
@@ -33,36 +46,74 @@ class ViewController: UIViewController, CLLocationManagerDelegate {
     }
     
     func startScanning() {
-        let uuid = UUID(uuidString: "52414449-553-4E45-5457-4F524B53434F")!
-        let beaconRegion = CLBeaconIdentityConstraint(uuid: uuid, major: 0, minor: 0)
-        locationManager?.startUpdatingLocation()
-        locationManager?.startRangingBeacons(satisfying: beaconRegion)
+        addBeacon(uuidString: "5A4BCFCE-174E-4BAC-A814-092E77F6B7E5", major: 123, minor: 456)
+        addBeacon(uuidString: "5A4BCFCE-174E-4BAC-A814-092E77F6B7E5", major: 123, minor: 456)
+        addBeacon(uuidString: "5A4BCFCE-174E-4BAC-A814-092E77F6B7E5", major: 123, minor: 456)
     }
     
-    func update(distance: CLProximity) {
-        UIView.animate(withDuration: 1) {
+    func addBeacon(uuidString: String, major: Int, minor: Int) {
+        let uuid = UUID(uuidString: uuidString)!
+        let constraint = CLBeaconIdentityConstraint(uuid: uuid)
+        let beaconRegion = CLBeaconRegion(beaconIdentityConstraint: constraint, identifier: uuid.uuidString)
+        
+        locationManager?.startMonitoring(for: beaconRegion)
+        locationManager?.startRangingBeacons(satisfying: constraint)
+    }
+    
+    func update(distance: CLProximity, name: String) {
+        UIView.animate(withDuration: 1) { [weak self] in
+            
+            self?.nameLabel.text = "\(name)"
+            
             switch distance {
             case .immediate:
-                self.view.backgroundColor = .red
-                self.distanceReading.text = "RIGHT HERE"
+                self?.view.backgroundColor = .red
+                self?.distanceReading.text = "RIGHT HERE"
+                self?.circleView.transform = CGAffineTransform(scaleX: 1, y: 1)
             case .near:
-                self.view.backgroundColor = .orange
-                self.distanceReading.text = "NEAR"
+                self?.view.backgroundColor = .orange
+                self?.distanceReading.text = "NEAR"
+                self?.circleView.transform = CGAffineTransform(scaleX: 0.5, y: 0.5)
             case .far:
-                self.view.backgroundColor = .blue
-                self.distanceReading.text = "FAR"
+                self?.view.backgroundColor = .blue
+                self?.distanceReading.text = "FAR"
+                self?.circleView.transform = CGAffineTransform(scaleX: 0.25, y: 0.25)
             default:
-                self.view.backgroundColor = .gray
-                self.distanceReading.text = "UNKNOWN"
+                self?.view.backgroundColor = .gray
+                self?.distanceReading.text = "UNKNOWN"
+                self?.nameLabel.text = "NO BEACON FOUND"
+                self?.circleView.transform = CGAffineTransform(scaleX: 0.001, y: 0.001)
             }
         }
     }
     
-    func locationManager(_ manager: CLLocationManager, didRange beacons: [CLBeacon], satisfying beaconConstraint: CLBeaconIdentityConstraint) {
+    // Challenge 1
+    func showAlert() {
+        if beaconDetected {
+            beaconDetected = false
+            let ac = UIAlertController(title: "Beacon detected", message: nil, preferredStyle: .alert)
+            ac.addAction(UIAlertAction(title: "OK", style: .default))
+            present(ac, animated: true)
+        }
+    }
+    
+    func locationManager(_ manager: CLLocationManager, didRangeBeacons beacons: [CLBeacon], in region: CLBeaconRegion) {
         if let beacon = beacons.first {
-            update(distance: beacon.proximity)
+            
+            // Challenge 2
+            if currentUUID == nil {
+                currentUUID = region.uuid
+            }
+            guard currentUUID == region.uuid else { return }
+            update(distance: beacon.proximity, name: region.identifier)
+            
+            // Challenge 1
+            showAlert()
         } else {
-            update(distance: .unknown)
+            // Challenge 2
+            guard currentUUID == region.uuid else { return }
+            currentUUID = nil
+            update(distance: .unknown, name: "NO BEACON FOUND")
         }
     }
 }
