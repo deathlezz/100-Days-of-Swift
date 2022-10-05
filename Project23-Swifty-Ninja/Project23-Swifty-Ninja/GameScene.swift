8114e7661e88dd45b9c9e257c855b3882a0db1dc
deathlezz <deathlezz@wp.pl>
2022-10-05 12:45:23 +0100

    Setting up

---
+++ Project23-Swifty-Ninja/Project23-Swifty-Ninja/GameScene.swift
@@ -0,0 +1,15 @@
+//
+//  GameScene.swift
+//  Project23-Swifty-Ninja
+//
+//  Created by deathlezz on 05/10/2022.
+//
+
+import SpriteKit
+
+class GameScene: SKScene {
+    
+    override func didMove(to view: SKView) {
+    }
+
+}
