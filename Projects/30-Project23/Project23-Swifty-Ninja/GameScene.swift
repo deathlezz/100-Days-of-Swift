05fd4dff8aa99b122871b11dd2078ba25d225c76
deathlezz <deathlezz@wp.pl>
2022-10-07 19:31:11 +0100

    Game over, man: SKTexture

--- Projects/30-Project23/Project23-Swifty-Ninja/GameScene.swift
+++ Projects/30-Project23/Project23-Swifty-Ninja/GameScene.swift
@@ -43,6 +43,8 @@ class GameScene: SKScene {
     var chainDelay = 3.0
     var nextSequenceQueued = true
     
+    var isGameEnded = false
+    
     override func didMove(to view: SKView) {
         let background = SKSpriteNode(imageNamed: "sliceBackground")
         background.position = CGPoint(x: 512, y: 384)
@@ -107,6 +109,8 @@ class GameScene: SKScene {
     }
     
     override func touchesMoved(_ touches: Set<UITouch>, with event: UIEvent?) {
+        guard isGameEnded == false else { return }
+        
         guard let touch = touches.first else { return }
         let location = touch.location(in: self)
         activeSlicePoints.append(location)
@@ -168,13 +172,26 @@ class GameScene: SKScene {
                 }
                 
                 run(SKAction.playSoundFileNamed("explosion", waitForCompletion: false))
-                endGame()
+                endGame(triggeredByBomb: true)
             }
         }
     }
     
-    func endGame() {
+    func endGame(triggeredByBomb: Bool) {
+        guard isGameEnded == false else { return }
         
+        isGameEnded = true
+        physicsWorld.speed = 0
+        isUserInteractionEnabled = false
+        
+        bombSoundEffect?.stop()
+        bombSoundEffect = nil
+        
+        if triggeredByBomb {
+            livesImages[0].texture = SKTexture(imageNamed: "sliceLifeGone")
+            livesImages[1].texture = SKTexture(imageNamed: "sliceLifeGone")
+            livesImages[2].texture = SKTexture(imageNamed: "sliceLifeGone")
+        }
     }
     
     func playSwooshSound() {
@@ -271,7 +288,6 @@ class GameScene: SKScene {
                 enemy.addChild(emitter)
             }
                 
-                
         } else {
             enemy = SKSpriteNode(imageNamed: "penguin")
             run(SKAction.playSoundFileNamed("launch", waitForCompletion: false))
@@ -305,7 +321,25 @@ class GameScene: SKScene {
     }
     
     func subtractLife() {
+        lives -= 1
         
+        run(SKAction.playSoundFileNamed("wrong", waitForCompletion: false))
+        
+        var life: SKSpriteNode
+        
+        if lives == 2 {
+            life = livesImages[0]
+        } else if lives == 1 {
+            life = livesImages[1]
+        } else {
+            life = livesImages[2]
+            endGame(triggeredByBomb: false)
+        }
+        
+        life.texture = SKTexture(imageNamed: "sliceLifeGone")
+        life.xScale = 1.3
+        life.yScale = 1.3
+        life.run(SKAction.scale(to: 1, duration: 0.1))
     }
     
     override func update(_ currentTime: TimeInterval) {
@@ -356,6 +390,8 @@ class GameScene: SKScene {
     }
     
     func tossEnemies() {
+        guard isGameEnded == false else { return }
+        
         popupTime *= 0.991
         chainDelay *= 0.99
         physicsWorld.speed *= 1.02
