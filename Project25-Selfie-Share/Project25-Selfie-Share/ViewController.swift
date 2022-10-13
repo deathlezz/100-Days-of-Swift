e8642ac17281c04daea277a2c59b791e0bf1acd3
deathlezz <deathlezz@wp.pl>
2022-10-13 09:36:29 +0100

    Setting up

---
+++ Project25-Selfie-Share/Project25-Selfie-Share/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Project25-Selfie-Share
+//
+//  Created by deathlezz on 13/10/2022.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
