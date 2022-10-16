88306cbb3bd7385f00329d02a646e834cad52ad0
deathlezz <deathlezz@wp.pl>
2022-10-16 12:18:19 +0100

    Setting up

---
+++ Project26-Marble-Maze/Project26-Marble-Maze/GameScene.swift
@@ -0,0 +1,16 @@
+//
+//  GameScene.swift
+//  Project26-Marble-Maze
+//
+//  Created by deathlezz on 16/10/2022.
+//
+
+import SpriteKit
+
+class GameScene: SKScene {
+    
+    override func didMove(to view: SKView) {
+        
+    }
+ 
+}
