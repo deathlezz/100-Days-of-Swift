599ef80fab8621492fecec44f9ce290f3fc18a65
deathlezz <deathlezz@wp.pl>
2022-10-20 11:56:44 +0100

    Drawing into a Core Graphics context with UIGraphicsImageRenderer

--- Projects/35-Project27/Project27-Core-Graphics/ViewController.swift
+++ Projects/35-Project27/Project27-Core-Graphics/ViewController.swift
@@ -9,11 +9,46 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var imageView: UIImageView!
+    var currentDrawType = 0
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
+        
+        drawRectangle()
     }
 
-
+    @IBAction func redrawTapped(_ sender: Any) {
+        currentDrawType += 1
+        
+        if currentDrawType > 5 {
+            currentDrawType = 0
+        }
+        
+        switch currentDrawType {
+        case 0:
+            drawRectangle()
+        default:
+            break
+        }
+    }
+    
+    func drawRectangle() {
+        let renderer = UIGraphicsImageRenderer(size: CGSize(width: 512, height: 512))
+        
+        let image = renderer.image { ctx in
+            // awesome drawing code
+            let rectangle = CGRect(x: 0, y: 0, width: 512, height: 512)
+            ctx.cgContext.setFillColor(UIColor.red.cgColor)
+            ctx.cgContext.setStrokeColor(UIColor.black.cgColor)
+            ctx.cgContext.setLineWidth(10)
+            ctx.cgContext.addRect(rectangle)
+            ctx.cgContext.drawPath(using: .fillStroke)
+        }
+        
+        imageView.image = image
+    }
+    
 }
 
