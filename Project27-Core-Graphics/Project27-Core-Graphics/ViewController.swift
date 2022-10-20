c3d075f7493ca9fa3e64bfcb7a13d721eb610bf8
deathlezz <deathlezz@wp.pl>
2022-10-20 10:20:12 +0100

    Setting up

---
+++ Project27-Core-Graphics/Project27-Core-Graphics/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Project27-Core-Graphics
+//
+//  Created by deathlezz on 20/10/2022.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
