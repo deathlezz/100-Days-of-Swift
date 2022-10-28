39325cdfcffcfc9d18a4358c1e5548343a5ed498
deathlezz <deathlezz@wp.pl>
2022-10-28 11:00:43 +0100

    Setting up

---
+++ Project28-Secret-Swift/Project28-Secret-Swift/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Project28-Secret-Swift
+//
+//  Created by deathlezz on 28/10/2022.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
