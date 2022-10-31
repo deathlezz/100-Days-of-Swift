7b8ecb00ba6c4fc294a61802763c69c9c83cf8a5
deathlezz <deathlezz@wp.pl>
2022-10-31 11:15:11 +0000

    Setting up

---
+++ Project29-Exploding-Monkeys/Project29-Exploding-Monkeys/GameScene.swift
@@ -0,0 +1,15 @@
+//
+//  GameScene.swift
+//  Project29-Exploding-Monkeys
+//
+//  Created by deathlezz on 31/10/2022.
+//
+
+import SpriteKit
+
+class GameScene: SKScene {
+    
+    override func didMove(to view: SKView) {
+
+    }
+}
